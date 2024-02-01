f0e5c7fa88ae3e79f8f196a30d26f83c691c073c
Vantech450 <150483313+Vantech450@users.noreply.github.com>
2024-02-01 21:39:34 +0800

    Delete backend directory

--- backend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    path('Vatrent/', admin.site.urls),
-    path('', include('base.urls')),
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
-]
